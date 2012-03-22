f427e88255ec6c20cca175526d9775ccc17a368f
James Current <dauthimaster@gmail.com>
2012-03-22 18:15:35 -0500

    commented Descriptor

--- src/Descriptor.java
+++ src/Descriptor.java
@@ -6,15 +6,23 @@
   Time: 2:50 PM
  */
 public class Descriptor {
-    private Type type;
+    private Type type;  //The type of the Descriptor
+
+    //Constructor. Return a new Descriptor with type type.
 
     public Descriptor(Type type){
         this.type = type;
     }
 
+    //getType. Return this Descriptor's type.
+
     public Type getType(){
         return type;
     }
 
-    //TODO: make toString method
+    //toString. Return a String that notates this Descriptor
+    
+    public String toString(){
+        return type.toString();
+    }
 }
